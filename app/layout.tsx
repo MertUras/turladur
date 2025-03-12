3ebfa0c9bada14174ae8269a3308c26375201c08
Dincer Velioglu <dincer.velioglu@gmail.com>
2025-03-13 01:03:50 +0300

    extensions changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/app/providers/theme-provider";
+import { ThemeProvider } from "./providers/theme-provider";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -15,9 +15,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="tr" suppressHydrationWarning>
       <body className={`${inter.variable} font-sans antialiased`}>
